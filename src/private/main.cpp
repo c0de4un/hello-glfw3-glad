de0fd9c828abfd805ca232ae0b5462c173a8e21a
c0de4un <code4un@yandex.ru>
2021-03-19 23:29:53 +0400

    hello utf8 world

---
+++ src/private/main.cpp
@@ -0,0 +1,73 @@
+﻿/**
+ * All rights reserved.
+ * License: see LICENSE.txt
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must display the names 'Denis Zyamaev' and
+ * in the credits of the application, if such credits exist.
+ * The authors of this work must be notified via email (code4un@yandex.ru) in
+ * this case of redistribution.
+ * 3. Neither the name of copyright holders nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// INCLUDES
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// Include STL
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+// Include Windows SDK
+#include <Windows.h>
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// TYPES
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#ifndef OK
+    static constexpr const int OK = 0;
+    #define OK OK
+#endif
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// MAIN
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+int main()
+{
+    // Set console code page to UTF-8 so console known how to interpret string data
+    SetConsoleOutputCP( CP_UTF8 );
+
+    // Enable buffering to prevent VS from chopping up UTF-8 byte sequences
+    setvbuf( stdout, nullptr, _IOFBF, 1000 );
+
+    // Output
+    const std::string str( u8"Привет мир !" );
+    std::cout << str << std::endl;
+    std::cin.get();
+
+    return OK;
+}
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
